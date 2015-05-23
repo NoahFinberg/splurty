a48335b3aec76f4c6c2570b5c0c7b6ed19a13fa2
Noah Finberg <noahfinberg@gmail.com>
2015-05-23 15:37:17 +0000

    write test, add unique_tag method, and implement unique tag feature

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,4 +4,16 @@ class Quote < ActiveRecord::Base
 	#meets presence and length requirements.
 	validates :saying, :presence => true, :length => { :maximum => 140, :minimum => 3 }
 	validates :author, :presence => true, :length => { :maximum => 50, :minimum => 3 }
+
+
+	##converts a name and quote id into the desired format. (Ex: author: Noah Finberg quote_id: 1 => 'NF#1')
+	def unique_tag
+
+		##collect transforms given array into the new array abbr
+		abbr = self.author.split(" ").collect do |sub_string|
+			sub_string[0] #puts first letter of each substring into intials array
+		end
+
+		return abbr.join + '#' + self.id.to_s ##join concatenates an array into a string
+	end
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,8 +2,14 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
 
-  ## test that always passes
-  test "the truth" do
-    assert true
-  end
+	test "unique_quote" do
+		quote = Quote.create(:author => 'Noah Finberg', :saying => 'Get out the way.') ## add something to test database
+		# puts quote.inspect ##helpful for debugging--outputs the value of the var at given time when run test r
+
+		expected = 'NF#' + quote.id.to_s ##what we want the output to be
+		actual = quote.unique_tag ##output of calling the method we are testing
+		assert_equal expected, actual ##makes sure what we want is what we get
+	end
+  
+
 end
